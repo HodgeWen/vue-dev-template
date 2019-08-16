cdaaa7e036fcc00a50d0b033ae2fc664ced8e8b2
whj <emailatwen@gmail.com>
2019-08-16 14:42:17 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import components from 'components'
-import 'iview/dist/styles/iview.css' // iview样式
 import 'utils/date'
+import 'style/normalize.less'
+import 'style/iview-overwrite'
 
 Vue.use(components)
 
